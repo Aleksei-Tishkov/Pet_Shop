215d69b4e9a2a04da9140dd4767d1e56adde6b27
Aleksei Tishkov <tscenic.ua@gmail.com>
2023-08-31 15:54:58 +0530

    Init

--- blog_engine/views.py
+++ blog_engine/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 
 def post_list(request):
-    return HttpResponse('<h1>Hi once again!</h1>')
+    return render(request, 'blog/index.html')
