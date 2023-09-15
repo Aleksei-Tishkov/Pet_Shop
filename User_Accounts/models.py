a43646c4ef54c7fcca21226845374555f6057506
Aleksei Tishkov <tscenic.ua@gmail.com>
2023-09-15 20:33:36 +0530

    User model updated

--- User_Accounts/models.py
+++ User_Accounts/models.py
@@ -3,10 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    def create_user(self, username, first_name, last_name, email, password):
+    def create_user(self, username, email, password):
         self.username = username
-        self.first_name = first_name
-        self.last_name = last_name
         self.email = email
         self.set_password(password)
         self.save()
