0f21ee272e2c3c05a73469c78a4b359ce10a20b9
Aleksei Tishkov <tscenic.ua@gmail.com>
2023-10-11 16:41:45 +0400

    Home_Page GSAP, SmoothScroll, ScrollTrigger applied

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -21,16 +21,56 @@ if (ScrollTrigger.isTouch !== 1) {
 
     })
 
-    gsap.fromTo('.doggo', {}, {
+    gsap.fromTo('.doggo', { }, {
         
         scrollTrigger: {
             trigger: '.hero-section',
             start: 'center',
+            end: '600',
 
             scrub: true
         }
 
     })
 
+    gsap.fromTo('.main_header', { }, {
+        
+        scrollTrigger: {
+            trigger: '.main_header',
+            start: 'center',
+            end: '-300',
+    
+            scrub: true
+        }
+
+    })
+
+    let itemsL = gsap.utils.toArray('.left__block .left__item')
+
+    itemsL.forEach(item => {
+        gsap.fromTo(item, { x: -100 }, {
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                scrub: true
+            }
+    
+        })
+    })
+
+    let itemsR = gsap.utils.toArray('.right__block .right__item')
+
+    itemsR.forEach(item => {
+        gsap.fromTo(item, { x: -100 }, {
+            x: 0,
+            scrollTrigger: {
+                trigger: item,
+                scrub: true
+            }
+    
+        })
+    })
+
+    
 
 }
